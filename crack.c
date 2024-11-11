f7af6dd1e5d9976e4644fa125c2c0cd52a2d8013
Lucas Lek <llek@student.sierracollege.edu>
2024-11-10 23:18:05 -0800

    Update crack.c

--- crack.c
+++ crack.c
@@ -3,14 +3,10 @@
 #include <string.h>
 
 #include "md5.h"
-
-#if __has_include("fileutil.h")
 #include "fileutil.h"
-#endif
 
 #define PASS_LEN 50     // Maximum length any password will be.
 #define HASH_LEN 33     // Length of hash plus one for null.
-
 
 int main(int argc, char *argv[])
 {
@@ -20,29 +16,42 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    // TODO: Read the hashes file into an array.
-    //   Use either a 2D array or an array of arrays.
-    //   Use the loadFile function from fileutil.c
-    //   Uncomment the appropriate statement.
+    // Read the hashes file into an array.
     int size;
-    //char (*hashes)[HASH_LEN] = loadFile(argv[1], &size);
-    //char **hashes = loadFile(argv[1], &size);
-    
-    // CHALLENGE1: Sort the hashes using qsort.
-    
-    // TODO
+    char **hashes = loadFileAA(argv[1], &size);
+
     // Open the password file for reading.
+    
+	FILE *passwords = fopen(argv[2], "r");
+	if (!passwords)
+	{
+	    perror("Can't open file");
+	    exit(1);
+	}
+    
+	char string[PASS_LEN];
+    int hashesFound = 0;
+	while (fgets(string, PASS_LEN, passwords) != NULL)
+	{
+		// trim the newline
+		for (int i = 0; i < strlen(string); i++)
+			if (string[i] == '\n')
+				string[i] = '\0';
 
-    // TODO
-    // For each password, hash it, then use the array search
-    // function from fileutil.h to find the hash.
-    // If you find it, display the password and the hash.
-    // Keep track of how many hashes were found.
-    // CHALLENGE1: Use binary search instead of linear search.
+        // hash the string and search for it
+        char *hash = md5(string, strlen(string));
+		char *found = substringSearchAA(hash, hashes, size);
+        if (found)
+        {
+            printf("Hash found: %s\n", found);
+            hashesFound++;
+        }
+    }
 
-    // TODO
-    // When done with the file:
-    //   Close the file
-    //   Display the number of hashes found.
-    //   Free up memory.
+    // Close the file
+	fclose(passwords);
+    // Display the number of hashes found.
+    printf("Hashes found: %d\n", hashesFound);
+    // Free up memory.
+    freeAA(hashes, size);
 }
